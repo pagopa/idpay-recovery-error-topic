d6b2aa9fe06c8adb2ccc3241bac6af5d1822b13b
sbellezz <121178708+sbellezz@users.noreply.github.com>
2023-07-05 10:22:24 +0200

    feat: IDP-992_migrazione_springboot_3 (#33)
    * [IDP-992] scanning CVE
    
    * [IDP-992] semantic versioning
    
    * [IDP-992] springboot 3 migration
    
    * [IDP-992] update dependencies
    
    * [IDP-992] fix jms publisher connection
    
    ---------
    
    Co-authored-by: sbellezz <sbellezz@emeal.nttdata.com>

--- src/main/java/it/gov/pagopa/idpay/error_recovery/producer/factory/JmsPublisherFactoryServiceImpl.java
+++ src/main/java/it/gov/pagopa/idpay/error_recovery/producer/factory/JmsPublisherFactoryServiceImpl.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.idpay.error_recovery.producer.factory;
 
-import com.azure.spring.cloud.autoconfigure.jms.ServiceBusJmsConnectionFactory;
-import com.azure.spring.cloud.autoconfigure.jms.properties.AzureServiceBusJmsProperties;
+import com.azure.spring.cloud.autoconfigure.implementation.jms.properties.AzureServiceBusJmsProperties;
+import com.azure.spring.cloud.autoconfigure.implementation.properties.core.authentication.TokenCredentialConfigurationProperties;
+import com.azure.spring.cloud.core.implementation.connectionstring.ServiceBusConnectionString;
+import com.azure.spring.jms.ServiceBusJmsConnectionFactory;
 import it.gov.pagopa.idpay.error_recovery.config.HandledPublishersConfig;
 import it.gov.pagopa.idpay.error_recovery.producer.JmsPublisher;
 import org.springframework.jms.core.JmsMessagingTemplate;
@@ -12,6 +14,7 @@ import java.util.Map;
 @Service
 public class JmsPublisherFactoryServiceImpl implements JmsPublisherFactoryService {
 
+    private static final String AMQP_URI_FORMAT = "amqps://%s?amqp.idleTimeout=%d";
     private final HandledPublishersConfig handledPublishersConfig;
     private final AzureServiceBusJmsProperties defaultServiceBusJmsProperties;
 
@@ -25,8 +28,9 @@ public class JmsPublisherFactoryServiceImpl implements JmsPublisherFactoryServic
         Map<String, String> producerProperties = handledPublishersConfig.getServiceBusPublisherProperties(srcServer, srcTopic);
 
         if (producerProperties != null) {
-            AzureServiceBusJmsProperties properties = buildProperties(defaultServiceBusJmsProperties, producerProperties);
-            ServiceBusJmsConnectionFactory connectionFactory = new ServiceBusJmsConnectionFactory(properties.getUsername(), properties.getPassword(), properties.getRemoteUrl());
+            ServiceBusConnectionString serviceBusConnectionString = new ServiceBusConnectionString(getConnectionString(defaultServiceBusJmsProperties, producerProperties));
+            AzureServiceBusJmsProperties properties = buildProperties(serviceBusConnectionString, defaultServiceBusJmsProperties, producerProperties);
+            ServiceBusJmsConnectionFactory connectionFactory = new ServiceBusJmsConnectionFactory(properties.getCredential().getUsername(), properties.getCredential().getPassword(), buildRemoteURI(serviceBusConnectionString, properties));
             connectionFactory.setClientID(properties.getTopicClientId());
             JmsMessagingTemplate jmsMessagingTemplate = new JmsMessagingTemplate(connectionFactory);
             jmsMessagingTemplate.setDefaultDestinationName(srcTopic);
@@ -36,17 +40,31 @@ public class JmsPublisherFactoryServiceImpl implements JmsPublisherFactoryServic
         }
     }
 
-    private static AzureServiceBusJmsProperties buildProperties(AzureServiceBusJmsProperties defaultProps, Map<String, String> overrides) {
+    private static AzureServiceBusJmsProperties buildProperties(ServiceBusConnectionString serviceBusConnectionString, AzureServiceBusJmsProperties defaultProps, Map<String, String> overrides) {
         AzureServiceBusJmsProperties properties = new AzureServiceBusJmsProperties();
-        properties.setConnectionString(overrides.getOrDefault("connection-string", defaultProps.getConnectionString()));
+        properties.setConnectionString(getConnectionString(defaultProps, overrides));
         properties.setPricingTier(overrides.getOrDefault("pricing-tier", defaultProps.getPricingTier()));
         properties.setTopicClientId(overrides.getOrDefault("topic-client-id", defaultProps.getTopicClientId()));
         try {
             properties.afterPropertiesSet();
+
+            properties.setCredential(new TokenCredentialConfigurationProperties());
+            properties.getCredential().setUsername(serviceBusConnectionString.getSharedAccessKeyName());
+            properties.getCredential().setPassword(serviceBusConnectionString.getSharedAccessKey());
         } catch (Exception e) {
             throw new IllegalArgumentException("Something gone wrong while configuring ServiceBus properties", e);
         }
         return properties;
+    }
+
+    private static String getConnectionString(AzureServiceBusJmsProperties defaultProps, Map<String, String> overrides) {
+        return overrides.getOrDefault("connection-string", defaultProps.getConnectionString());
+    }
+
+    private static String buildRemoteURI(ServiceBusConnectionString serviceBusConnectionString, AzureServiceBusJmsProperties properties) {
+        String host = serviceBusConnectionString.getEndpointUri().getHost();
+
+        return String.format(AMQP_URI_FORMAT, host, properties.getIdleTimeout().toMillis());
     }
 
 }
