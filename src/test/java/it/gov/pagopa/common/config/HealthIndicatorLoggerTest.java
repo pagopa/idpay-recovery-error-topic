5dc42e01259cf570b247d9500e03167d36e90039
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2026-04-13 16:49:31 +0200

    update spring boot to version 4 and java to version 25

--- src/test/java/it/gov/pagopa/common/config/HealthIndicatorLoggerTest.java
+++ src/test/java/it/gov/pagopa/common/config/HealthIndicatorLoggerTest.java
@@ -1,66 +1,92 @@
 package it.gov.pagopa.common.config;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import it.gov.pagopa.common.utils.MemoryAppender;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointAutoConfiguration;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.health.Status;
-import org.springframework.boot.actuate.system.DiskSpaceHealthIndicator;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
+import org.springframework.boot.health.contributor.Status;
 
+import java.util.List;
 import java.util.Map;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {HealthIndicatorLogger.class, HealthEndpointAutoConfiguration.class})
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class HealthIndicatorLoggerTest {
 
-    @MockBean
-    private DiskSpaceHealthIndicator diskSpaceHealthIndicatorMock;
     private MemoryAppender memoryAppender;
+    private HealthIndicatorLogger healthIndicatorLogger;
 
-    @Autowired
-    private HealthEndpoint healthEndpoint;
+    private HealthIndicator diskSpaceIndicator;
+    private HealthIndicator dbIndicator;
 
     @BeforeEach
-    public void setup() {
-        ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(HealthIndicatorLogger.class.getName());
+    void setUp() {
+        Logger logger = (Logger) LoggerFactory.getLogger(HealthIndicatorLogger.class);
         memoryAppender = new MemoryAppender();
         memoryAppender.setContext((LoggerContext) LoggerFactory.getILoggerFactory());
-        logger.setLevel(ch.qos.logback.classic.Level.INFO);
-        logger.addAppender(memoryAppender);
+        memoryAppender.setName("MEMORY");
         memoryAppender.start();
 
-        Mockito.when(diskSpaceHealthIndicatorMock.health())
-                .thenReturn(Health.down().withDetails(Map.of("DETAILKEY", "DETAILVALUE")).build());
+        logger.setLevel(Level.INFO);
+        logger.addAppender(memoryAppender);
+
+        diskSpaceIndicator = Mockito.mock(HealthIndicator.class);
+        dbIndicator = Mockito.mock(HealthIndicator.class);
+
+        healthIndicatorLogger = new HealthIndicatorLogger(
+                List.of(diskSpaceIndicator, dbIndicator)
+        );
     }
+
     @Test
-    void testDown(){
-        test(Status.DOWN);
+    void shouldLogWhenStatusIsDown() {
+        Mockito.when(diskSpaceIndicator.health())
+                .thenReturn(Health.down()
+                        .withDetails(Map.of("DETAILKEY", "DETAILVALUE"))
+                        .build());
+
+        Mockito.when(dbIndicator.health())
+                .thenReturn(Health.up().build());
+
+        Health result = healthIndicatorLogger.health();
+
+        assertEquals(Status.UP, result.getStatus());
+        assertEquals(1, memoryAppender.getLoggedEvents().size());
     }
 
     @Test
-    void testOutOfService(){
-        Mockito.when(diskSpaceHealthIndicatorMock.health())
-                .thenReturn(Health.outOfService().withDetails(Map.of("DETAILKEY", "DETAILVALUE")).build());
+    void shouldLogWhenStatusIsOutOfService() {
+        Mockito.when(diskSpaceIndicator.health())
+                .thenReturn(Health.outOfService()
+                        .withDetails(Map.of("DETAILKEY", "DETAILVALUE"))
+                        .build());
 
-        test(Status.OUT_OF_SERVICE);
+        Mockito.when(dbIndicator.health())
+                .thenReturn(Health.up().build());
+
+        Health result = healthIndicatorLogger.health();
+
+        assertEquals(Status.UP, result.getStatus());
+        assertEquals(1, memoryAppender.getLoggedEvents().size());
     }
 
-    private void test(Status expectedStatus) {
-        healthEndpoint.health();
+    @Test
+    void shouldNotLogWhenAllIndicatorsAreUp() {
+        Mockito.when(diskSpaceIndicator.health())
+                .thenReturn(Health.up().build());
 
-        Assertions.assertEquals(1, memoryAppender.getLoggedEvents().size());
-        Assertions.assertEquals("[HEALTH][%s] DiskSpaceHealthIndicator: {DETAILKEY=DETAILVALUE}".formatted(expectedStatus),
-                memoryAppender.getLoggedEvents().get(0).getFormattedMessage());
+        Mockito.when(dbIndicator.health())
+                .thenReturn(Health.up().build());
+
+        Health result = healthIndicatorLogger.health();
+
+        assertEquals(Status.UP, result.getStatus());
+        assertEquals(0, memoryAppender.getLoggedEvents().size());
     }
 }
--- src/test/java/it/gov/pagopa/idpay/error_recovery/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/idpay/error_recovery/BaseIntegrationTest.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.data.mongodb.test.autoconfigure.AutoConfigureDataMongo;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -45,7 +45,6 @@ import java.util.stream.StreamSupport;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
-@EnableAutoConfiguration()
 @EmbeddedKafka(topics = {
         "${errorListener.topic}",
         "idpay-onboarding-outcome",
@@ -67,7 +66,6 @@ import static org.awaitility.Awaitility.await;
                 "spring.kafka.consumer.bootstrap-servers=${spring.embedded.kafka.brokers}",
                 "handled-publishers.kafka.idpay-evh-ns-00.properties.bootstrap.servers=${spring.embedded.kafka.brokers}",
                 "handled-publishers.kafka.idpay-evh-ns-01.properties.bootstrap.servers=${spring.embedded.kafka.brokers}",
-                //endregion
 
                 //region service bus
                 // mocked replacing it using kafka
@@ -148,7 +146,7 @@ public abstract class BaseIntegrationTest {
             kafkaBroker.addTopics(topic);
         }
 
-        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(groupId, "true", kafkaBroker);
+        Map<String, Object> consumerProps = KafkaTestUtils.consumerProps(kafkaBroker, groupId, true);
         consumerProps.put("key.deserializer", StringDeserializer.class);
         DefaultKafkaConsumerFactory<String, String> cf = new DefaultKafkaConsumerFactory<>(consumerProps);
         Consumer<String, String> consumer = cf.createConsumer();
